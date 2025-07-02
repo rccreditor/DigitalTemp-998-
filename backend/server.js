7fe752228b11a9b89dfd2b9ca1dc4722d7aa8f59
Your Name <you@example.com>
2025-07-02 09:31:28 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require("./passport")
-require('dotenv').config();
 
 const authRoutes = require('./routes/auth');
 
